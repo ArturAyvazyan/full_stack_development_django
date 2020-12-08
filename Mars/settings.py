d19568ebe79ab1682d060aad66759bc2a51d04d6
Artur <52126850+ArturAyvazyan@users.noreply.github.com>
2020-12-09 01:46:00 +0300

    ready to run

--- Mars/settings.py
+++ Mars/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['margarits-porcelain.herokuapp.com']
 
 
 # Application definition
