e1c695641aa52b0d0b3116c9e55f301cc1e37d6a
Артур Айвазян <archiforeverything@gmail.com>
2021-04-26 01:22:30 +0300

    updated models and views to zayvka head page

--- glavnaya/views.py
+++ glavnaya/views.py
@@ -109,7 +109,7 @@ def golova(request):
             otpravka = request.POST.get('otpravka')
             payment = request.POST.get('payment')
             koment = request.POST.get('koment', False)
-
+            db_entry = Zakaz(work= {golova}, name = {zayavka_name}, email = {zayavka_email}, city = {zayavka_city}, otpravka = {otpravka}, payment = {payment}, koment = {koment})
             message = f"""Заявка на покупку работы: {golova},
             ИМЯ: {zayavka_name}, 
             e-mail: {zayavka_email}, 
@@ -118,10 +118,12 @@ def golova(request):
             Оплата следующим способом: {payment},
             Комментарий к заказу: {koment}."""
 
+
             
             if zayavka_name == False:
                 return render(request, 'works_desktop/golova.html', {'golova':golova})
-            else:        
+            else:
+                db_entry.save()        
                 send_mail (
                     golova, #subject
                     message, #message
